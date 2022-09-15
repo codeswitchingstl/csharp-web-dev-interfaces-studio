d1f776551f68dcae8104192f0f492c038032fb0a
codeswitchingstl <codeswitching@icloud.com>
2022-09-14 20:35:04 -0500

    initial commit

---
+++ SpinningDiscs/BaseDisc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpinningDiscs
+{
+    public abstract class BaseDisc
+    {
+        public string Name { get; set; }
+        public int StorageCapacity { get; set; }
+        public int StorageUsed { get; set; }
+        public int StorageRemaining { get; set; }
+        public string Contents { get; set; }
+        public string DiscType { get; set; }
+
+        public BaseDisc(string name, int storageCapacity, string discType)
+        {
+            Name = name;
+            StorageCapacity = storageCapacity;
+            //Contents = contents;
+            DiscType = discType;
+        }
+    }
+}
